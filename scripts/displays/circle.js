dca0a4d6a98c809fd726d99d1c95083d5409f3b1
Jim Challenger <jim.challenger1@gmail.com>
2013-08-23 21:13:17 -0500

    Two player enabled offline, arrow and wasd to control.

--- scripts/displays/circle.js
+++ scripts/displays/circle.js
@@ -12,28 +12,47 @@ re.circle(10, "#00CCFF");
 
 */
 re.c('circle')
-.factory(function(radius, color){
+.factory(function(radius, color, keytype){
 	//this is called when using re.circle()
 	this.radius = radius;
 	this.color = color;
+	this.keytype = keytype;
 })
 .requires('align update')
 .defines({
 	//define properties
   speed:15,
-  color:'#ff0000'
+  color:'#ff0000',
+  keytype:"arrow"
 })
 .events({
 
 	//adds a listener for updates
 	update:function(){
 
-    //move on keypressed
-    if(re.pressed('a', 'left')) this.posX -= this.speed;
-    if(re.pressed('d', 'right')) this.posX += this.speed;
-    
-    if(re.pressed('w', 'up')) this.posY -= this.speed;
-    if(re.pressed('s', 'down')) this.posY += this.speed;
+		//move for arrows
+		if(this.keytype == "arrow")
+		{
+			if(re.pressed('left')) this.posX -= this.speed;
+	    if(re.pressed('right')) this.posX += this.speed;
+	    
+	    if(re.pressed('up')) this.posY -= this.speed;
+	    if(re.pressed('down')) this.posY += this.speed;
+		}
+
+		//move for wasd
+		if(this.keytype == "wasd")
+		{
+	    if(re.pressed('a')) this.posX -= this.speed;
+	    if(re.pressed('d')) this.posX += this.speed;
+	    
+	    if(re.pressed('w')) this.posY -= this.speed;
+	    if(re.pressed('s')) this.posY += this.speed;			
+		}
+		
+
+
+
     
 	}
 
--- scripts/scenes/home.js
+++ scripts/scenes/home.js
@@ -5,20 +5,22 @@ re.scene('home')
   re.preventDefault('left right up down');
   
   var radius = 10;
-  var color = '#FF0000';
+  var red = '#FF0000';
+  var blue = "#0000FF"
 
   //create new circle on canvas
-  re.circle(radius, color)
+  re.circle(radius, red, 'arrow')
+  re.circle(radius, blue, 'wasd')
   //from align component
   .alignHor()
   .alignVer();
 
   //find circle and change speed
   re('circle').first().attr('speed', 15);
-  
+  re('circle')[1].attr('speed', 15);
   //add help text
   re.e('text align')
-  .text('Use WASD or arrow keys to move the circle')
+  .text('Use WASD to move blue and arrow keys to move red')
   .alignTop(5)
   .alignLeft(5);
   
